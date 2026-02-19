4354a006ed5c63178d8ad799c1715670355db683
codecraft-sanju <sanjusirvich18@gmail.com>
2026-02-19 18:41:25 +0530

    fff

--- SMSSlaveApp/react-native.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  dependencies: {
-    'react-native-get-sms-android': {
-      platforms: {
-        android: null, // Yeh line autolinking ko Android ke liye band kar degi
-      },
-    },
-  },
-};
